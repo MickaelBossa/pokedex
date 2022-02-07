c005bffacb89ec7f80f2dd98a0335fea4262231c
Mickael Bossa <mickael.bossa@outlook.com>
2022-02-07 17:37:30 +0100

    Add typescript and the list of pokemons

--- src/App.js
+++
@@ -1,9 +0,0 @@
-// Librairies
-import React from 'react';
-import './App.css';
-
-function App() {
-  return <div className='App'></div>;
-}
-
-export default App;
---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+// Librairies
+import React, { FunctionComponent, useState, useEffect } from 'react';
+
+// Content 
+import Pokemon from './models/pokemon';
+import POKEMONS from './models/mock-pokemon';
+
+// Composants
+import PokemonCard from './components/Pokemon-card';
+  
+const App: FunctionComponent = () => {
+ // States
+ const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+ // useEffect
+  useEffect(() => {
+    setPokemons(POKEMONS);
+  }, []);
+    
+  return (
+    <div>
+      <h1 className="center">Pokédex</h1>
+      <div className="container"> 
+        <div className="row"> 
+        {pokemons.map(pokemon => (
+          <PokemonCard key={pokemon.id} pokemon={pokemon}/>
+        ))}
+        </div>
+      </div>
+    </div> 
+  );
+}
+  
+export default App;
